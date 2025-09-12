ebaa07ea2083ddaa736dbbfe40413795ac702702
Shruthi Prabhu <mshruprabhu@gmail.com>
2025-09-12 17:30:55 +0200

    Add CloudFormation output for ECR repository URI

--- my-cdk-app/lib/ecr.ts
+++ my-cdk-app/lib/ecr.ts
@@ -9,9 +9,16 @@ export class EcrStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create the ECR repository
     this.repository = new ecr.Repository(this, 'MyRepository', {
       repositoryName: 'my-app-repo',
       imageScanOnPush: true,
+    });
+
+    // ✅ Add a CloudFormation output to expose the URI
+    new cdk.CfnOutput(this, 'ECRRepoURI', {
+      value: this.repository.repositoryUri,
+      exportName: 'ECRRepoURI', // optional, can be used for cross-stack references
     });
   }
 }
