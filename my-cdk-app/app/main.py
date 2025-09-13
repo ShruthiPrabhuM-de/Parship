c26918f977e76ab826dec6271590dc31bb27287c
Shruthi Prabhu <mshruprabhu@gmail.com>
2025-09-13 12:20:44 +0200

    FIX: Deleted destroy job

--- my-cdk-app/app/main.py
+++ my-cdk-app/app/main.py
@@ -1,15 +1,18 @@
-from typing import Union
-
 from fastapi import FastAPI
+from fastapi.testclient import TestClient
 
 app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_main():
+    return {"msg": "Hello World"}
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+client = TestClient(app)
+
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"msg": "Hello World"}
