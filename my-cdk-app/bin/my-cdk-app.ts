6a95132191d14fafe460eebfc6469a7624507d21
Shruthi Prabhu <mshruprabhu@gmail.com>
2025-09-15 00:59:25 +0200

    FEAT: Updating Iam stack

--- my-cdk-app/bin/my-cdk-app.ts
+++ my-cdk-app/bin/my-cdk-app.ts
@@ -1,22 +1,25 @@
-// bin/app.ts
 import * as cdk from 'aws-cdk-lib';
 import { EcrStack } from '../lib/ecr';
 import { EcsClusterStack } from '../lib/ecs-cluster';
 import { EcsServiceStack } from '../lib/ecs-service';
 import { AlbStack } from '../lib/alb';
-
+import { ItAdminIamStack } from '../lib/iam';
 
 const app = new cdk.App();
 
+// Configure IAM for itadmin
+new ItAdminIamStack(app, 'ItAdminIamStack', {
+  itAdminUserName: 'itadmin',
+});
 
+// Other stacks
 const ecrStack = new EcrStack(app, 'EcrStack');
 const clusterStack = new EcsClusterStack(app, 'EcsClusterStack');
 const ecsServiceStack = new EcsServiceStack(app, 'EcsServiceStack', {
   cluster: clusterStack.cluster,
-  repository: ecrStack.repository, 
+  repository: ecrStack.repository,
 });
 
-// NEW: Add AlbStack
 new AlbStack(app, 'AlbStack', {
   vpc: clusterStack.cluster.vpc,
   service: ecsServiceStack.service,
